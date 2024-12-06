cf4de3cbd39fcd024aa882ac7e26b69ffb2e2065
Seweryn Czykinowski <sevveryn.czykinowski@gmail.com>
2024-12-06 07:18:12 +0100

    end

--- src/components/badge.tsx
+++ src/components/badge.tsx
@@ -3,14 +3,12 @@
 import badge_captcha from '@/assets/badges/badge_captcha.svg'
 import badge_cookie_fail from '@/assets/badges/badge_cookie_fail.svg'
 import badge_quiz_main from '@/assets/badges/badge_quiz_main.svg'
-import badge_quiz_pod_1 from '@/assets/badges/badge_quiz_pod_1.svg'
-import badge_quiz_pod_2 from '@/assets/badges/badge_quiz_pod_2.svg'
 import badge_read from '@/assets/badges/badge_read.svg'
 import badge_retro from '@/assets/badges/badge_retro.svg'
 import Image from 'next/image'
 import { useEffect, useState } from 'react'
 
-export type BadgeName = 'Captcha' | 'Cookie Fail' | 'Quiz Main' | 'Quiz Pod 1' | 'Quiz Pod 2' | 'Read' | 'Retro'
+export type BadgeName = 'Captcha' | 'Cookie Fail' | 'Quiz Main' | 'Read' | 'Retro'
 
 interface Badge {
     title: BadgeName;
@@ -21,8 +19,6 @@ const BADGES: Badge[] = [
     { title: 'Captcha', icon: badge_captcha },
     { title: 'Cookie Fail', icon: badge_cookie_fail },
     { title: 'Quiz Main', icon: badge_quiz_main },
-    { title: 'Quiz Pod 1', icon: badge_quiz_pod_1 },
-    { title: 'Quiz Pod 2', icon: badge_quiz_pod_2 },
     { title: 'Read', icon: badge_read },
     { title: 'Retro', icon: badge_retro },
 ]
